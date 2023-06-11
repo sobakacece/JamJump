edbc546c89c5d4198a8535dd0594c48e6fa9b718
Humane Tiger <121488080+HumaneTiger@users.noreply.github.com>
2023-06-11 19:21:22 +0200

    small color adjustment

--- UI/HUD.cs
+++ UI/HUD.cs
@@ -3,30 +3,30 @@ using System;
 
 public class HUD : CanvasLayer
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-    [Export] Label scoreLabel;
-    Camera2D camera;
-    float currentScore = 0;
-    float highestScore = 0;
-    ScreenManager screenManager;
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        camera = GetNode<Camera2D>("/root/TestLevel/Camera2D");
-        scoreLabel = GetNode<Label>("VBoxContainer/Label");
-        screenManager = GetNode<ScreenManager>("/root/ScreenManager");
-    }
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	[Export] Label scoreLabel;
+	Camera2D camera;
+	float currentScore = 0;
+	float highestScore = 0;
+	ScreenManager screenManager;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		camera = GetNode<Camera2D>("/root/TestLevel/Camera2D");
+		scoreLabel = GetNode<Label>("VBoxContainer/Label");
+		screenManager = GetNode<ScreenManager>("/root/ScreenManager");
+	}
 
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-        currentScore = -camera.GlobalPosition.y;
-        if (currentScore > highestScore)
-            highestScore = currentScore;
+	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		currentScore = -camera.GlobalPosition.y;
+		if (currentScore > highestScore)
+			highestScore = currentScore;
 
-        scoreLabel.Text = ((highestScore).ToString("00000000"));
-        screenManager.MyFinalScore = highestScore;
-    }
+		scoreLabel.Text = ((highestScore).ToString("00000000"));
+		screenManager.MyFinalScore = highestScore;
+	}
 }
